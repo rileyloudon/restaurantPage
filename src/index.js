cdcbf91f18200c872b49c627fb2ad01cd5997e90
Riley Loudon <rileyloudon@icloud.com>
2021-01-29 21:10:17 -0600

    display initial-load on startup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('tester');
+import initialLoad from './modules/initial-load';
+
+initialLoad();
+
+import homepage from './modules/homepage';
+import menu from './modules/menu';
+import contact from './modules/contact';
+
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const contactBtn = document.getElementById('contact');
+
+homeBtn.addEventListener('click', homepage);
+menuBtn.addEventListener('click', menu);
+contactBtn.addEventListener('click', contact);
