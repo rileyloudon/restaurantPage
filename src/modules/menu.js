5e4677d2b496163b1b86cc97dcaf2c8ceb00c846
Riley Loudon <rileyloudon@icloud.com>
2021-02-01 20:48:07 -0600

    style menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -8,21 +8,33 @@ const menu = () => {
     <p>Each item comes with your choice of tea, coffee, or water.</p>
       <ul>
         <li>
-          <h3>Cream Stew</h3>
-          <img src='/dist/img/cream-stew.png' alt='Cream Stew'>
-          <p>Brock's Cream Stew includes carrots, broccoli, potato chunks, some chicken, and a little bit of yellow onion. Made fresh outdoors on a wood fire.</p>
+          <div class='menu-item'>
+            <h3>Cream Stew</h3>
+            <div class='description'>
+              <img src='img/cream-stew.png' alt='Cream Stew'>
+              <p>Brock's Cream Stew includes carrots, broccoli, potato chunks, some chicken, and a little bit of yellow onion. Made fresh outdoors on a wood fire.</p>
+            </div>
+          </div>
         </li>
 
         <li>
-          <h3>Onigiri/Rice Balls</h3>
-          <img src='/dist/img/rice_balls.png' alt='Cream Stew'>
-          <p>Brock's Onigiri is made using rice rolled into a triangle shape. Filled with your choice of salmon, chicken, tuna, shrimp, or pickled plums.</p>
+          <div class='menu-item'>
+            <h3>Onigiri/Rice Balls</h3>
+              <div class='description'>
+                <img src='img/rice_balls.png' alt='Onigiri/Rice Balls'>
+                <p>Brock's Onigiri is made using rice rolled into a triangle shape. Filled with your choice of salmon, chicken, tuna shrimp, or pickled plums.</p>
+            </div>
+          </div>
         </li>
 
         <li>
-          <h3>Jelly Donuts</h3>
-          <img src='/dist/img/jelly-donut.jpg' alt='Cream Stew'>
-          <p>Brock's Jelly Donuts may look identical to the Onigiri, but they are nothing a like. These are real donuts filled with jelly then topped with powdered sugar and coconut.</p>
+          <div class='menu-item'>
+            <h3>Jelly Donuts</h3>
+            <div class='description'>
+              <img src='img/jelly-donut.jpg' alt='Jelly Donuts'>
+              <p>Brock's Jelly Donuts may look identical to the Onigiri, but they are nothing a like. These are real donuts filled with jelly then topped with powdered sugar and coconut.</p>
+            </div>
+          </div>
         </li>
       </ul>
     </div>`;
