25c4b4e514b2d8d9f910763da441b46398e20345
Riley Loudon <rileyloudon@icloud.com>
2021-02-02 23:43:07 -0600

    add placeholders to inputs and give submit an ID

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -4,19 +4,19 @@ const contact = () => {
   main.innerHTML = `
   <img src='img/contact.jpg' alt='Picture of Brock' class='home-img'>
   <div id='contact'>
-  <h1>Contact</h1>
-    <p>Each message can be replied by email or hand delivered by our Dragonite.</p>
-    <form>
+    <h1>Contact</h1>
+    <p>Each message can be replied by email or hand delivered by our friendly Dragonite.</p>
+    <form id='contact-form'>
       <label for='name'>Name:</label>
-      <input type='text' id='name' name='name'>
+      <input type='text' id='name' name='name' placeholder='Name'>
 
       <label for='reply'>Email OR Address:</label>
-      <input type='text' id='reply' name='reply'>
+      <input type='text' id='reply' name='reply' placeholder='Email/Address'>
 
       <label for='message'>Message:</label>
-      <textarea id='message' name='message' rows='4' cols='25'></textarea>
+      <textarea id='message' name='message' rows='5' cols='35' placeholder='Enter Message'></textarea>
 
-      <input type='submit' value='Submit'>
+      <input type='submit' value='Submit' id='submit'>
     </form>
   </div>
   `;
